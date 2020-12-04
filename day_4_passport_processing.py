395b1854af5bd8d4e583ce17f23920ae88683098
MikeWannaHockALoogie <amwhite4@gmail.com>
2020-12-04 12:31:29 -0800

    git ignore/ refactored day 4 /improved commenting

--- day_4_passport_processing.py
+++ day_4_passport_processing.py
@@ -1,9 +1,3 @@
-'''
-pseudo:
-1.) load input into pandas df 
-2.) select data with appropriate fields as not Null 
-
-'''
 import pandas as pd
 import re
 file = 'day_4_passport_processing_input.txt'
@@ -15,6 +9,9 @@ def load_data(file)->list:
     passports ={'byr':[] ,'iyr':[] ,'eyr':[] ,'hgt':[] ,'hcl':[] ,'ecl':[] ,'pid':[] ,'cid':[] }
 
     with open(file) as f:
+        '''
+        read input data into a dict and return pandas data frame 
+        '''
         f= f.read().split('\n\n')
         # split entries where wever thier is a double line break
         for entry in f:
@@ -36,10 +33,6 @@ def load_data(file)->list:
                     passports[key].append(None)
         return pd.DataFrame.from_dict(passports)
     
-
-
-
-
 data = load_data(file)
 # select data for solution one by excluding 'cid' and dropping any null values 
 l = [x for x in data.columns[:-1]]
@@ -48,19 +41,17 @@ s1 = s1.dropna()
  # select data for solution two with logic from readme.txt    
 s2 =s1[
     s1.byr.between('1920','2002')
-    & s1.iyr.between('2010','2020', inclusive = True)
-    & s1.eyr.between('2020','2030', inclusive = True)
-    & (
-        s1.hgt.str.fullmatch('(15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-6])cm') | s1.hgt.str.match('(59|6[0-9]|7[0-6])in') 
-    )
+    & s1.iyr.between('2010','2020')
+    & s1.eyr.between('2020','2030')
+    & s1.hgt.str.fullmatch('(1[5-8]\d|19[0-6])cm|(59|6\d|7[0-6])in') 
     & s1.hcl.str.fullmatch('#[0-9a-f]{6}')
     & s1.ecl.isin(['amb','blu','brn','gry','grn','hzl','oth'])
     & s1.pid.str.fullmatch('\d{9}') 
 
 ]
 # solutions for part one and two 
-print(s1.shape)
-print(s2.shape)
+print(s1.shape[0])
+print(s2.shape[0])
 
 
 
